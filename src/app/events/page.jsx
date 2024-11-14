f7eeb97ffb76598107469f7471c6f41d2d0d6967
mir-asif01 <asifbsb2002@gmail.com>
2024-11-14 18:12:34 +0600

    all events page structure added

--- src/app/events/page.jsx
+++ src/app/events/page.jsx
@@ -1,7 +1,51 @@
+import { Link } from "lucide-react"
+
 export default function Events() {
   return (
-    <section>
-      <h1>all events created by you</h1>
+    <section className="py-10 px-20">
+      <h1 className="text-3xl text-center mb-20">all events created by you</h1>
+      <div className="grid grid-cols-4 items-center justify-center gap-3">
+        <div className="border border-gray-300 bg-white hover:bg-neutral-50 cursor-pointer rounded-lg hover:shadow px-4 py-2 text-center">
+          <h1 className="text-xl font-semibold border-b mb-2">Event title</h1>
+          <p className="text-justify">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore eos
+            consequatur beatae dolores voluptatum corporis magni, culpa sit
+            fugit nostrum.
+          </p>
+        </div>
+        <div className="border border-gray-300 bg-white hover:bg-neutral-50 cursor-pointer rounded-lg hover:shadow px-4 py-2 text-center">
+          <h1 className="text-xl font-semibold border-b mb-2">Event title</h1>
+          <p className="text-justify">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore eos
+            consequatur beatae dolores voluptatum corporis magni, culpa sit
+            fugit nostrum.
+          </p>
+        </div>
+        <div className="border border-gray-300 bg-white hover:bg-neutral-50 cursor-pointer rounded-lg hover:shadow px-4 py-2 text-center">
+          <h1 className="text-xl font-semibold border-b mb-2">Event title</h1>
+          <p className="text-justify">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore eos
+            consequatur beatae dolores voluptatum corporis magni, culpa sit
+            fugit nostrum.
+          </p>
+        </div>
+        <div className="border border-gray-300 bg-white hover:bg-neutral-50 cursor-pointer rounded-lg hover:shadow px-4 py-2 text-center">
+          <h1 className="text-xl font-semibold border-b mb-2">Event title</h1>
+          <p className="text-justify">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore eos
+            consequatur beatae dolores voluptatum corporis magni, culpa sit
+            fugit nostrum.
+          </p>
+        </div>
+        <div className="border border-gray-300 bg-white hover:bg-neutral-50 cursor-pointer rounded-lg hover:shadow px-4 py-2 text-center">
+          <h1 className="text-xl font-semibold border-b mb-2">Event title</h1>
+          <p className="text-justify">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore eos
+            consequatur beatae dolores voluptatum corporis magni, culpa sit
+            fugit nostrum.
+          </p>
+        </div>
+      </div>
     </section>
   )
 }
