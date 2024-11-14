e78a9bcb23720e42fe786de87ad66cf14b06f1e7
mir-asif01 <asifbsb2002@gmail.com>
2024-11-14 11:04:21 +0600

    problem we solve section added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/HeroSection"
 import Pricing from "@/components/Pricing"
+import ProblemsWeSolve from "@/components/ProblemsWeSolve"
 
 export default function Home() {
   return (
     <main className="mt-16">
       <HeroSection />
       <Pricing />
+      <ProblemsWeSolve />
     </main>
   )
 }
---
+++ src/components/ProblemsWeSolve.jsx
@@ -0,0 +1,53 @@
+export default function ProblemsWeSolve() {
+  return (
+    <section className="p-20">
+      <h1 className="text-5xl text-center mb-20">
+        Problem we are trying to solve
+      </h1>
+      <div className="flex justify-around items-center gap-5">
+        <div className="flex flex-col justify-center items-center gap-5">
+          <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 cursor-pointer">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              easier participant management
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center gap-5">
+          <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 cursor-pointer">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              ticket distributions made easy
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+          </div>
+          <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 cursor-pointer">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              easy acces to events
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center gap-5">
+          <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 cursor-pointer">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              personalized dashboard for organizers
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
